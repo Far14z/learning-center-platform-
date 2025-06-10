e4dc3bc9c2ab0c77f750a694b126ec0fd609b34f
Far14z <faridce14@gmail.com>
2025-06-10 16:43:19 -0500

    feat(publishing): add outbound service contract.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Repositories/ITutorialRepository.cs
@@ -0,0 +1,11 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Aggregates;
+using ACME.LearningCenterPlatform.API.Shared.Domain.Repositories;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Repositories;
+
+public interface ITutorialRepository : IBaseRepository<Tutorial>
+{
+    Task<IEnumerable<Tutorial>> FindByCategoryIdAsync(int CategoryId);
+
+    Task<bool> ExistsTitleAsync(string title);
+}
